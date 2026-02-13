8289dd673bc0b3df58b7dcdfd4940ad16e30921b
Rohit Patel <rp380972@gmail.com>
2026-02-13 22:04:23 +0530

    Added a Signals for auto profile creation using django signal

---
+++ accounts/signals.py
@@ -0,0 +1,15 @@
+# Auto Profile Creation Signals
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import User, StudentProfile, InstructorProfile
+
+@receiver(post_save, sender = User)
+def create_user_profile(sender, instance, created, **kwargs) : 
+    if created : 
+        if instance.role == User.STUDENT : 
+            StudentProfile.objects.create(user = instance)
+        
+        elif instance.role == User.INSTRUCTOR : 
+            InstructorProfile.objects.create(user = instance)
+        
